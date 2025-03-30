286bc05271d68c91bb4e778405cbcf5a05e4e670
eco1018 <dealasadduq@gmail.com>
2025-03-30 15:10:36 +0300

    Initial Commit

---
+++ Aura_iOSTests/Aura_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Aura_iOSTests.swift
+//  Aura_iOSTests
+//
+//  Created by Ella A. Sadduq on 3/30/25.
+//
+
+import Testing
+@testable import Aura_iOS
+
+struct Aura_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
